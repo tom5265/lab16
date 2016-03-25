86b181b3014c6a01d0578eebc1e0395d1026ce5d
Tom Moore <tom5265@gmail.com>
2016-03-25 10:32:24 -0500

    fixed nav on creating new post

--- app/src/viewcontrols/composepostviewcontrol/composepostviewcontrol.vc.ts
+++ app/src/viewcontrols/composepostviewcontrol/composepostviewcontrol.vc.ts
@@ -1,6 +1,7 @@
 import {register} from 'platypus';
 import BaseViewControl from '../base/base.vc';
 import BlogService from '../../services/blogservice/blogservice.svc';
+import PostListViewControl from '../postlistviewcontrol/postlistviewcontrol.vc';
 
 export default class ComposePostViewControl extends BaseViewControl {
     templateString: string = require('./composepostviewcontrol.vc.html');
@@ -22,8 +23,9 @@ constructor(private blogSvc: BlogService) {
     
    send() {
        let message = {title: this.context.title, author: this.context.author, content: this.context.content};
-       
        this.blogSvc.sendPost(message);
+       this.navigator.navigate(PostListViewControl);
+       
    }
    
 }
